015a438e76e3fa1506535a06ec871cc07165b6e8
brettrijnders <brettrijnders@83ffc965-6a71-c195-7b19-07ce798467d5>
2010-07-05 15:12:06 +0000

    copyright year update

--- trunk/PBSViewer/inc/CB_guidID.inc.php
+++ trunk/PBSViewer/inc/CB_guidID.inc.php
@@ -1,4 +1,32 @@
 <?php
+/*
+    This program 'PBSViewer' also known as Punkbuster Screenshot Viewer, 
+    will download pb screens. Those downloaded screens are published on
+    your website.
+    
+    Copyright (C) 2010  B.S. Rijnders aka BandAhr
+
+    PBSViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PBSViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PBSViewer.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ____________________
+    contact information:
+    --------------------
+    mail:		brettrijnders@gmail.com
+    website:	http://www.beesar.com       
+
+*/
+
 // those guidIDs were taken from http://clanbase.ggl.com/personlist.php
 // manual update might be necessary
 // those guidIDS will be used for finding more info about player on clanbase
