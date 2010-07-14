0b60213f65641a7064cf8aa6dc46e6551faa8d00
brettrijnders <brettrijnders@83ffc965-6a71-c195-7b19-07ce798467d5>
2010-07-14 20:10:14 +0000

    - Added Zoom feature for screen shots - Added 'Save As...' feature for screen shots

---
+++ PBSViewer/inc/imgSave.inc.php
@@ -0,0 +1,52 @@
+<?php
+/*
+    This program 'PBSViewer' also known as Punkbuster Screenshot Viewer, 
+    will download pb screens. Those downloaded screens are published on
+    your website.
+    
+    Copyright (C) 2010  B.S. Rijnders aka BandAhr
+
+    PBSViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PBSViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PBSViewer.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ____________________
+    contact information:
+    --------------------
+    mail:		brettrijnders@gmail.com
+    website:	http://www.beesar.com       
+
+*/
+
+// This file is used to save images
+
+if (isset($_GET['saveIMG']))
+{
+	$file = '../download/'.$_GET['saveIMG'].'.png';
+
+	if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
+}
+
+}
+?>
