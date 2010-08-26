ae86555da244b51dae71e4f683d76f8fb88b0506
brettrijnders <brettrijnders@83ffc965-6a71-c195-7b19-07ce798467d5>
2010-08-26 13:48:11 +0000

    Implemented issue 13, now you can make image brighter or darker

--- trunk/PBSViewer/inc/GD/GD_ajax.inc.php
+++ trunk/PBSViewer/inc/GD/GD_ajax.inc.php
@@ -6,83 +6,83 @@ function_exists("gd_info")? $gd=true:$gd=false;
 if($gd)
 {
 	
+	$download_dir	=	'../../download/';
 
-$download_dir	=	'../../download/';
+	if(isset($_GET['imgfid']))
+	{
+		$im = imagecreatefrompng($download_dir.$_GET['imgfid'].'.png');	
 
-
-if(isset($_GET['imgfid']))
-{
-	$im = imagecreatefrompng($download_dir.$_GET['imgfid'].'.png');	
-
-
-if(isset($_GET['gammaOut']))
-{
+		if(isset($_GET['gammaOut']))
+		{
 		
-$gammaOut = $_GET['gammaOut'];
+			$gammaOut = $_GET['gammaOut'];
 
-if ($gammaOut<0) $gammaOut =0.1;
+			if ($gammaOut<=0) $gammaOut =0;
 
-// Correct gamma, out = 1.537
-imagegammacorrect($im, 1.0, $gammaOut);
+			echo $gammaOut;
+			
+			// Correct gamma, out = 1.537
+			imagegammacorrect($im, 1.0, $gammaOut);
 
-// Save and free image
-imagepng($im, $download_dir.'temp.png');
-imagedestroy($im);
+			// Save and free image
+			imagepng($im, $download_dir.'temp.png');
+			imagedestroy($im);
+			
+			$IMGsrc = 'download/temp.png?'.time();
+			list($widthIMG, $heightIMG, $typeIMG, $attrIMG) = getimagesize($IMGsrc);
+   			echo "<img src=\"".$IMGsrc."\" style=\"width:".$widthIMG."px; height: ".$heightIMG."px;\" onmouseover=\"TJPzoomif(this);\" id=\"unique1337\" alt=\"".$fid.'png'."\">";
 
-}
-elseif (isset($_GET['negate']))
-{
+		}
+		elseif (isset($_GET['negate']))
+		{
 	
-	if($_GET['negate']==1)
-	{
+			if($_GET['negate']==1)
+			{
 	
-	function negate($im)
-{
-    if(function_exists('imagefilter'))
-    {
-        return imagefilter($im, IMG_FILTER_NEGATE);
-    }
+				function negate($im)
+				{
+    				if(function_exists('imagefilter'))
+    				{
+        				return imagefilter($im, IMG_FILTER_NEGATE);
+    				}
 
-    for($x = 0; $x < imagesx($im); ++$x)
-    {
-        for($y = 0; $y < imagesy($im); ++$y)
-        {
-            $index = imagecolorat($im, $x, $y);
-            $rgb = imagecolorsforindex($index);
-            $color = imagecolorallocate($im, 255 - $rgb['red'], 255 - $rgb['green'], 255 - $rgb['blue']);
+    				for($x = 0; $x < imagesx($im); ++$x)
+    				{
+        				for($y = 0; $y < imagesy($im); ++$y)
+        				{
+            				$index = imagecolorat($im, $x, $y);
+            				$rgb = imagecolorsforindex($index);
+            				$color = imagecolorallocate($im, 255 - $rgb['red'], 255 - $rgb['green'], 255 - $rgb['blue']);
 
-            imagesetpixel($im, $x, $y, $color);
-        }
-    }
+            				imagesetpixel($im, $x, $y, $color);
+        				}
+    				}
 
-    return(true);
-}
+    				return(true);
+				}
 
-if($im && negate($im))
-{
-    //echo 'Image successfully converted to negative colors.';
-  
-    imagepng($im, $download_dir.'temp.png');
-    imagedestroy($im);
+				if($im && negate($im))
+				{
+    				imagepng($im, $download_dir.'temp.png');
+    				imagedestroy($im);
     
-    $IMGsrc = 'download/temp.png';
-    list($widthIMG, $heightIMG, $typeIMG, $attrIMG) = getimagesize($IMGsrc);
-    echo "
-    <img src=\"".$IMGsrc."\" style=\"width:".$widthIMG."px; height: ".$heightIMG."px;\" onmouseover=\"TJPzoomif(this);\" id=\"unique1337\" alt=\"".$fid.'png'."\">";
-}
-else
-{
-    echo 'Converting to negative colors failed.';
-}
-	}
-	else 
-	{
-		$IMGsrc = 'download/'.$_GET['imgfid'].'.png';
-    	list($widthIMG, $heightIMG, $typeIMG, $attrIMG) = getimagesize($IMGsrc);
-   	 	echo "<img src=\"".$IMGsrc."\" style=\"width:".$widthIMG."px; height: ".$heightIMG."px;\" onmouseover=\"TJPzoomif(this);\" id=\"unique1337\" alt=\"".$fid.'png'."\">";
-	}
-}
+    				$IMGsrc = 'download/temp.png?'.time();
+    				list($widthIMG, $heightIMG, $typeIMG, $attrIMG) = getimagesize($IMGsrc);
+	    			echo "<img src=\"".$IMGsrc."\" style=\"width:".$widthIMG."px; height: ".$heightIMG."px;\" onmouseover=\"TJPzoomif(this);\" id=\"unique1337\" alt=\"".$fid.'png'."\">";
+				}
+				else
+				{
+    				echo 'Converting to negative colors failed.';
+				}
+			}
+			else 
+			{
+				$IMGsrc = 'download/'.$_GET['imgfid'].'.png?'.time();
+    			list($widthIMG, $heightIMG, $typeIMG, $attrIMG) = getimagesize($IMGsrc);
+   	 			echo "<img src=\"".$IMGsrc."\" style=\"width:".$widthIMG."px; height: ".$heightIMG."px;\" onmouseover=\"TJPzoomif(this);\" id=\"unique1337\" alt=\"".$fid.'png'."\">";
+			}
+		}
 
-}
+	}
 }
 ?>
